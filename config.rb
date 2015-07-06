f3232029247034615de43375ecd15913c9a29a85
Moaaz Sidat <moaaz.omer@gmail.com>
2015-07-06 00:34:59 -0400

    Added autoprefixer

--- config.rb
+++ config.rb
@@ -23,3 +23,21 @@ javascripts_dir = "javascripts"
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+require 'autoprefixer-rails'
+ 
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  map = file + '.map'
+ 
+  if File.exists? map
+    result = AutoprefixerRails.process(css,
+      from: file,
+      to:   file,
+      map:  { prev: File.read(map), inline: false })
+    File.open(file, 'w') { |io| io << result.css }
+    File.open(map,  'w') { |io| io << result.map }
+  else
+    File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
+  end
+end
